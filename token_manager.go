011ea933bc43c12e459bde250b73d230e4a86a15
punkystone <email@punkystone.de>
2025-06-15 11:17:56 +0200

    add isValid method

--- token_manager.go
+++ token_manager.go
@@ -1,5 +1,29 @@
 package manager
 
-func Test(value string) string {
-	return value
+import "net/http"
+
+type TokenManager struct {
+	clientID     string
+	clientSecret string
+}
+
+func NewTokenManager(clientID, clientSecret string) *TokenManager {
+	return &TokenManager{
+		clientID,
+		clientSecret,
+	}
+}
+
+func (manager *TokenManager) IsValid(accessToken string) (bool, error) {
+	request, err := http.NewRequest(http.MethodGet, "https://id.twitch.tv/oauth2/validate", nil)
+	if err != nil {
+		return false, err
+	}
+	request.Header.Add("Authorization", "Bearer "+accessToken)
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return false, err
+	}
+	defer response.Body.Close()
+	return response.StatusCode == http.StatusOK, nil
 }
