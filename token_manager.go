053009180d8dc3e6ab4ee0d8336ef86ee206c286
punkystone <email@punkystone.de>
2025-06-15 11:32:51 +0200

    add refresh method

--- token_manager.go
+++ token_manager.go
@@ -1,6 +1,11 @@
 package manager
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"strings"
+)
 
 type TokenManager struct {
 	clientID     string
@@ -26,4 +31,37 @@ func (manager *TokenManager) IsValid(accessToken string) (bool, error) {
 	}
 	defer response.Body.Close()
 	return response.StatusCode == http.StatusOK, nil
+}
+
+func (manager *TokenManager) RefreshToken(refreshToken string) (bool, string, string, error) {
+	data := url.Values{}
+	data.Add("client_id", manager.clientID)
+	data.Add("client_secret", manager.clientSecret)
+	data.Add("grant_type", "refresh_token")
+	data.Add("refresh_token", refreshToken)
+	response, err := http.Post("https://id.twitch.tv/oauth2/token", "application/x-www-form-urlencoded", strings.NewReader(data.Encode()))
+	if err != nil {
+		return false, "", "", err
+	}
+	if response.StatusCode == http.StatusBadRequest {
+		return false, "", "", nil
+	}
+	defer response.Body.Close()
+	tokenResponse := &TokenResponse{}
+	err = json.NewDecoder(response.Body).Decode(tokenResponse)
+	if err != nil {
+		return false, "", "", err
+	}
+	return true, tokenResponse.AccessToken, tokenResponse.RefreshToken, nil
+}
+
+func (manager *TokenManager) ValidateAndRefreshToken(accessToken string, refreshToken string) (bool, string, string, error) {
+	valid, err := manager.IsValid(accessToken)
+	if err != nil {
+		return false, accessToken, refreshToken, err
+	}
+	if valid {
+		return true, accessToken, refreshToken, nil
+	}
+	return manager.RefreshToken(refreshToken)
 }
---
+++ types.go
@@ -0,0 +1,6 @@
+package manager
+
+type TokenResponse struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+}
